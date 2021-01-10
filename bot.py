9f164d764d8dbe32b66f6887ee71ab12f5cb067e
Codeize <mrcodeize@gmail.com>
2021-01-10 18:26:07 +0000

    initial

---
+++ bot.py
@@ -0,0 +1,18 @@
+import praw
+from helper import read_id, read_secret, read_agent
+
+cid = read_id()
+secret = read_secret()
+agent = read_agent()
+
+reddit = praw.Reddit(
+     client_id=cid,
+     client_secret=secret,
+     user_agent=agent
+)
+
+sr = reddit.subreddit("ProgrammerHumor")
+
+for post in sr.hot(limit=10):
+    print("***************************************************************************")
+    print(post.title)
---
+++ helper.py
@@ -0,0 +1,14 @@
+def read_id():
+    with open("secrets.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+def read_secret():
+    with open("secrets.txt", "r") as f:
+        lines = f.readlines()
+        return lines[1].strip()
+
+def read_agent():
+    with open("secrets.txt", "r") as f:
+        lines = f.readlines()
+        return lines[2].strip()
